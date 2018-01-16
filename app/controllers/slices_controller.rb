9588afb1a6a1ee427f1d106a4245e87a41acafc2
Alex Sukov <sukovsasha@gmail.com>
2018-01-16 10:29:35 +0300

    Fixed Settings Form

--- app/controllers/slices_controller.rb
+++ app/controllers/slices_controller.rb
@@ -2,11 +2,11 @@ class SlicesController < ApplicationController
   before_action :set_slice, only: [:update, :destroy]
 
   def create
-    @slice = slice.new(slice_params)
+    @slice = Slice.new(slice_params)
 
     respond_to do |format|
       if @slice.save
-        format.html { redirect_to @slice, notice: 'slice was successfully created.' }
+        format.html { redirect_to root_path, notice: 'slice was successfully created.' }
         format.json { render :show, status: :created, location: @slice }
       else
         format.html { render :new }
@@ -20,7 +20,7 @@ class SlicesController < ApplicationController
   def update
     respond_to do |format|
       if @slice.update(slice_params)
-        format.html { redirect_to @slice, notice: 'slice was successfully updated.' }
+        format.html { redirect_to root_path, notice: 'slice was successfully updated.' }
         format.json { render :show, status: :ok, location: @slice }
       else
         format.html { render :edit }
@@ -36,7 +36,7 @@ class SlicesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_slice
-      @slice = slice.find(params[:id])
+      @slice = Slice.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
