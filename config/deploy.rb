eb6afb0d603f731275cdf59f69dc1a37849fb374
AlexSukov <sukovsasha@gmail.com>
2018-03-08 17:33:39 +0300

    Nginx Conf 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 # set :keep_releases, 5
 
 ## Linked Files & Directories (Default None):
-#set :linked_files, %w{config/database.yml config/secrets.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads}
 
 namespace :puma do
