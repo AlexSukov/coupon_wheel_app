548796534086d1291a1fa5c3a603eefe4ef0ca60
AlexSukov <sukovsasha@gmail.com>
2018-03-23 18:48:24 +0300

    Fixed ImageMagick

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,12 +4,12 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     :provider               => 'AWS',                             # required
-    :aws_access_key_id      => Rails.application.secrets.secret_key_aws_id,            # required
-    :aws_secret_access_key  => Rails.application.secrets.secret_key_aws_password,     # required
-    :region                 => 'eu-west-2'                        # optional, defaults to 'us-east-1'
+    :aws_access_key_id      => Rails.application.secrets.aws_access_key_id,            # required
+    :aws_secret_access_key  => Rails.application.secrets.aws_secret_access_key,     # required
+    :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'math4all'               # required
+  config.fog_directory  = 'zoomifi-exitstopper'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
-  config.fog_public     = false                                  # optional, defaults to true
+  config.fog_public     = true                                  # optional, defaults to true
   #config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 end
